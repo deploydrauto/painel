7e26fb46beac1ad08b85f5562d328823279b7dbd
serpams <eng.mserpa@gmail.com>
2022-12-11 10:29:25 -0300

    remove initial

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,11 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::get('/', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
