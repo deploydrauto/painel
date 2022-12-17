412122703139ef0b68ada0b7cba47ae6d4acf3a0
serpams <eng.mserpa@gmail.com>
2022-12-17 10:05:52 -0300

    add custom  cors

--- app/Http/Controllers/api/ClienteController.php
+++ app/Http/Controllers/api/ClienteController.php
@@ -1,6 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\api;
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
 
 use App\Http\Controllers\Controller;
 use App\Models\client_bots;
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
 
 use App\Http\Controllers\api\ClienteController;
 use App\Http\Controllers\WebhooksController;
