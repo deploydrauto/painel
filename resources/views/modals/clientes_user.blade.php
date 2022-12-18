d678353a282e90bd654632fa5a1388b9176eee56
serpams <eng.mserpa@gmail.com>
2022-12-18 11:57:01 -0300

    ajusta off

--- resources/views/modals/clientes_user.blade.php
+++ resources/views/modals/clientes_user.blade.php
@@ -10,8 +10,8 @@
                 </h5>
                 <div class="self-end">
                     <a class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
-                        data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
-                        aria-controls="offcanvasExample">
+                        data-bs-toggle="offcanvas" href="#clientEdit" role="button"
+                        aria-controls="clientEdit">
                         + Cliente
                     </a>
                 </div>
