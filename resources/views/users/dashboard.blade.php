28f6a1c8e56ba4bd766b231ec360accd062ca6b2
serpams <eng.mserpa@gmail.com>
2023-01-03 10:21:56 -0300

    Update dashboard.blade.php

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -99,7 +99,7 @@
                     @include('modals.clientes_user')
                     @include('modals.games_user')
                     @include('modals.webhooks_user')
-
+                    @include('modals.client_edit')
 
 
 
@@ -314,7 +314,7 @@
                         <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white cursor-pointer"
 
                         >       <a class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
-                        data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
+                        data-bs-toggle="offcanvas" href="#offcanvascliente" role="button"
                         aria-controls="clientEdit">
                             <p>Nome: ${client.nome}</p>
                             <p>Email: ${client.email}</p>
