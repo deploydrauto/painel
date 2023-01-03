64f81c41e8ecef26a8f87ead2a726a2f1e727182
serpams <eng.mserpa@gmail.com>
2023-01-03 10:23:29 -0300

    Update clientes_user.blade.php

--- resources/views/modals/clientes_user.blade.php
+++ resources/views/modals/clientes_user.blade.php
@@ -16,7 +16,7 @@
                     </a>
                 </div>
                 @include('modals.cliente_add');
-
+                @include('modals.client_edit')
                 <button type="button"
                     class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
                     data-bs-dismiss="modal" aria-label="Close"></button>
