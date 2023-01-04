e83831ef9ee28bb59139ccfb17c6b239e9019ed7
serpams <eng.mserpa@gmail.com>
2023-01-03 21:35:20 -0300

    Update cliente_add.blade.php

--- resources/views/modals/cliente_add.blade.php
+++ resources/views/modals/cliente_add.blade.php
@@ -76,9 +76,9 @@
                             id="exampleInput125" id="telefone" name="telefone" placeholder="Telefone address">
                     </div>
                     <div class="datepicker relative form-floating form-group mb-6" data-mdb-toggle-button="false">
-                        <input type="text"
+                        <input type="date"
                             class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                            placeholder="Select a date" data-mdb-toggle="datepicker" name="client_inicio" id="client_inicio"/>
+                            name="client_inicio" id="client_inicio"/>
                         <label for="floatingInput" class="text-gray-700">Data de Inicio</label>
                     </div>
                     <div class="form-group mb-6">
