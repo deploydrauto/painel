5bb74e6cc5a31de8a2269df44b0ce8c661aa9d23
serpams <eng.mserpa@gmail.com>
2023-01-05 18:06:19 -0300

    ajuste lay

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -333,7 +333,7 @@ selectElement.addEventListener('change', (event) => {
                 data.forEach(client => {
                     let row = document.createElement('tr');
                     row.innerHTML = `
-                        <td class="py-4 px-4 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white cursor-pointer " >
+                        <td class="max-w-[20%] py-4 px-4 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white cursor-pointer " >
                          <a class="inline-block px-4 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
                         data-bs-toggle="offcanvas" href="#offcanvascliente" role="button" onClick="showEditClient(${client.id})"
                         aria-controls="clientEdit">
