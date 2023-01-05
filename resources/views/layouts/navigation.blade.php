f1f81c88ba12c6861a981f41649b5a6abbb4accf
serpams <eng.mserpa@gmail.com>
2023-01-05 20:00:54 -0300

    ajusta

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
                         {{ __('Games') }}
                     </x-nav-link>
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                        {{ __('Usuarios') }}
+                        {{ __('Admins') }}
                     </x-nav-link>
                     @endcan
 
