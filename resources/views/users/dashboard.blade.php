3f11fab8b8c3161deafe800d2217d94655642aa9
serpams <eng.mserpa@gmail.com>
2023-01-05 18:08:48 -0300

    ajuste lay

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -334,7 +334,7 @@ selectElement.addEventListener('change', (event) => {
                     let row = document.createElement('tr');
                     row.innerHTML = `
                         <td style="width:100px;max-width:100px" class=" py-4 px-4 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white cursor-pointer " >
-                         <a class="inline-block px-4 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
+                         <a style="width:100px;max-width:100px" class="inline-block px-4 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
                         data-bs-toggle="offcanvas" href="#offcanvascliente" role="button" onClick="showEditClient(${client.id})"
                         aria-controls="clientEdit">
                             <p>Nome: ${client.nome}</p>
