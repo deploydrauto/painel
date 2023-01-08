4b4ed4de4e6e803ed96feb512a9e29abafec667a
serpams <eng.mserpa@gmail.com>
2023-01-07 23:06:56 -0300

    ajusta tdestroy

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -117,15 +117,7 @@
 
 <script>
     $(document).ready(function () {
-    $('#example').DataTable({
-        lengthMenu: [
-            [5 , 10, 25, 50,100,1000, -1],
-            [5 ,10, 25, 50, 100,1000, 'All'],
-        ],
-        paging: false,
-        stateSave: true,
-    "bDestroy": true
-    });
+    
 
     const selectElement = document.querySelector('.ice-cream');
 
@@ -338,16 +330,22 @@ $('#example').DataTable();
     function fetchUserClients(id) {
 
         document.getElementById('user_id').value = id;
-
+        
         fetch('/users/clientes/' + id)
             .then(response => response.json())
             .then(data => {
                 let table = document.getElementById('user-clients-table');
                 table.innerHTML = '';
-                console.log(data);
-               var t =  $('#example').DataTable();
-
-                data.forEach(client => {
+                var t =  $('#example').DataTable({
+                        lengthMenu: [
+                            [5 , 10, 25, 50,100,1000, -1],
+                            [5 ,10, 25, 50, 100,1000, 'All'],
+                        ],
+                    
+                    stateSave: true,
+                    "bDestroy": true
+                    });
+                 data.forEach(client => {
                     let row = document.createElement('tr');
                     t.row.add([`<a class="inline-block px-4 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-1.5"
                         data-bs-toggle="offcanvas" href="#offcanvascliente" role="button" onClick="showEditClient(${client.id})"
